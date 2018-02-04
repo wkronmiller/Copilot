00d9c5f24559fcd0441bb8d23bb52e2352a9a5c0
William Rory Kronmiller <wrk961@gmail.com>
2018-02-04 15:15:21 -0500

    Initial Commit

---
+++ iOSCopilot/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOSCopilot
+//
+//  Created by William Rory Kronmiller on 2/4/18.
+//  Copyright © 2018 William Rory Kronmiller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
