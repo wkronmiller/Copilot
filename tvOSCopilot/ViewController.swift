d1516148b05f630bb4136b303a15efb933d21ebf
William Rory Kronmiller <wrk961@gmail.com>
2018-07-06 22:05:50 -0400

    WIP: tvos support

---
+++ tvOSCopilot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tvOSCopilot
+//
+//  Created by William Rory Kronmiller on 7/6/18.
+//  Copyright © 2018 William Rory Kronmiller. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
